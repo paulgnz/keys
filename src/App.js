2bc0c630f212c8b15db7559ae1ed532494f6f386
Remy Chauveau <rem.chauveau@gmail.com>
2024-04-08 13:25:38 +0200

    Move GLTF model to app and provide scene to lock through props using the scene.clone() method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import Lock from './components/Lock';
-import Key from './components/Key';
-import Camera from './components/Camera';
-import { AxesHelper } from 'three';
+import "./App.css";
+import React from "react";
+import {Canvas} from "@react-three/fiber";
+import Lock from "./components/Lock";
+import Key from "./components/Key";
+import Camera from "./components/Camera";
+import {AxesHelper} from "three";
+import {useGLTF} from "@react-three/drei";
 
 function App() {
+  const locks = ["a", "b", "c"];
+  const {scene, nodes, materials} = useGLTF("/assets/Padlock.gltf");
+
   return (
     <Canvas>
       <Camera />
       <ambientLight intensity={3} />
       <pointLight position={[10, -10, 10]} />
-      <Lock id={1} position={[-2.5, 1, 0]} />
-      <Lock id={2} position={[0, 1, 0]} />
-      <Lock id={3} position={[2.5, 1, 0]} />
+
+      <Lock key={1} position={[-2.5, 1, 0]} model={scene.clone()} />
+      <Lock key={2} position={[0, 1, 0]} model={scene.clone()} />
+      <Lock key={3} position={[2.5, 1, 0]} model={scene.clone()} />
       <primitive object={new AxesHelper(5)} />
       <Key />
     </Canvas>
--- src/components/Lock.js
+++ src/components/Lock.js
@@ -1,41 +1,37 @@
-import React, { useState, useRef } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
-import { useSpring, a } from '@react-spring/three';
-import { useGLTF } from '@react-three/drei';
+import React, {useState, useRef} from "react";
 
+import {useSpring, a} from "@react-spring/three";
 
-function Lock({id, position, ...lockProps}) {
+function Lock({id, position, model, ...lockProps}) {
   const lockRef = useRef();
-  const { scene } = useGLTF('/assets/Padlock.gltf');
+
   //const { camera } = useThree();
   const [hovered, setHovered] = useState(false);
-  console.log('Lock id: ' + id)
+  console.log("Lock id: " + id);
 
   const scale = useSpring({
     scale: hovered ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    config: { tension: 280, friction: 60 },
+    config: {tension: 280, friction: 60},
   });
 
-  const handleClick = (e) => {
+  const handleClick = e => {
     e.stopPropagation();
-    console.log(id + ' Lock clicked');
-  }
+    console.log(id + " Lock clicked");
+  };
 
   return (
     <a.primitive
-    name={`Lock_${id}`}
-    ref={lockRef}
-    position={position}
-    object={scene}
-    onClick={handleClick}
-    onPointerOver={() => setHovered(true)}
-    onPointerOut={() => setHovered(false)}
-    scale={scale.scale}
-    {...lockProps}
-    >
-    </a.primitive>
+      name={`Lock_${id}`}
+      ref={lockRef}
+      position={position}
+      object={model}
+      onClick={handleClick}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      scale={scale.scale}
+      {...lockProps}
+    ></a.primitive>
   );
 }
 
-useGLTF.preload('/assets/Padlock.gltf'); // Preload the model
 export default Lock;
